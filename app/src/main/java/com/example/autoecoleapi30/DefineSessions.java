7c94d6e27319e47234084b0b1c6cda976db7be24
bensalemmariem <mariembensalem766@gmail.com>
2024-01-08 22:39:49 +0100

    ajout de classe code

--- app/src/main/java/com/example/autoecoleapi30/DefineSessions.java
+++ app/src/main/java/com/example/autoecoleapi30/DefineSessions.java
@@ -1,7 +1,11 @@
 
 package com.example.autoecoleapi30;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,84 @@ public class DefineSessions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_define_sessions);
+
+
+        // Liaison des vues du XML au code Java
+        View view = findViewById(R.id.view);
+        TextView textView = findViewById(R.id.textView);
+        TextView textViewTitle = findViewById(R.id.textViewTitle);
+        Button btnReserveLearningSession = findViewById(R.id.btnReserveLearningSession);
+        Button btnReserveCodeSession = findViewById(R.id.btnReserveCodeSession);
+        Button btnReserveParkingSession = findViewById(R.id.btnReserveParkingSession);
+
+        // Vous pouvez définir des actions pour les boutons ici, par exemple :
+        btnReserveLearningSession.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Actions à effectuer lors du clic sur le bouton de réservation d'une séance d'apprentissage
+
+
+
+            }
+        });
+        btnReserveLearningSession.setOnClickListener(v -> {
+            Intent intent;
+
+            intent = new Intent(DefineSessions.this, code.class);
+            startActivity(intent);
+
+
+        });
+
+
+// passer au interface de  réservation de seance de code
+
+        btnReserveCodeSession.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Actions à effectuer lors du clic sur le bouton de réservation d'une séance d'apprentissage
+
+
+
+            }
+        });
+        btnReserveCodeSession.setOnClickListener(v -> {
+            Intent intent;
+
+            intent = new Intent(DefineSessions.this, code.class);
+            startActivity(intent);
+
+
+        });
+    // passer au interface de réservation seance code
+
+        btnReserveParkingSession.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Actions à effectuer lors du clic sur le bouton de réservation d'une séance d'apprentissage
+
+
+
+            }
+        });
+        btnReserveParkingSession.setOnClickListener(v -> {
+            Intent intent;
+
+            intent = new Intent(DefineSessions.this, code.class);
+            startActivity(intent);
+
+
+        });
+
+
+
+
+
+
+
+
+
+
     }
+
 }
---
+++ app/src/main/java/com/example/autoecoleapi30/code.java
@@ -0,0 +1,13 @@
+package com.example.autoecoleapi30;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class code extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.code);
+}}
